702ff183aac821b895baf4805b6eb55f0f51658a
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-03-18 14:38:03 -0600

    Spring Boot MVC project created

---
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/MeanGirlsAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpameangirls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeanGirlsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeanGirlsAppApplication.class, args);
+	}
+
+}
---
+++ MeanGirlsApp/src/test/java/com/skilldistillery/jpameangirls/MeanGirlsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpameangirls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeanGirlsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
