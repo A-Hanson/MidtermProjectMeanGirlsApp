5cc2b6795c8600416f91947a8687503159b0802c
Anna Hanson <>
2021-03-20 11:12:51 -0600

    add stub for Clique

--- JPAMeanGirls/src/test/java/com/skilldistillery/jpameangirls/entities/CliqueTest.java
+++ JPAMeanGirls/src/test/java/com/skilldistillery/jpameangirls/entities/CliqueTest.java
@@ -2,6 +2,9 @@ package com.skilldistillery.jpameangirls.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,7 +12,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CliqueTest {
-
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Clique clique;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
