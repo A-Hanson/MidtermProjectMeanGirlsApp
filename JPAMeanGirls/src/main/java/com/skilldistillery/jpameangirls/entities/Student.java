f22cbd9bc1ca49df9a644558dac5d7aeb28d6f9d
Sofia <sgkartist@gmail.com>
2021-03-20 11:58:46 -0600

    Student entity created and tested.

---
+++ JPAMeanGirls/src/main/java/com/skilldistillery/jpameangirls/entities/Student.java
@@ -0,0 +1,137 @@
+package com.skilldistillery.jpameangirls.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+//	@Column(name="user_id")
+//	private int userId;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="grade_level")
+	private Integer gradeLevel;
+	
+	@Column(name="created_date")
+	private LocalDateTime createdDate;
+	
+	@Column(name="birthday_date")
+	private LocalDateTime birthdayDate;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+	
+	
+	// constructors
+	
+	public Student() {}
+
+	// getters and setters
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getGradeLevel() {
+		return gradeLevel;
+	}
+
+	public void setGradeLevel(Integer gradeLevel) {
+		this.gradeLevel = gradeLevel;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getBirthdayDate() {
+		return birthdayDate;
+	}
+
+	public void setBirthdayDate(LocalDateTime birthdayDate) {
+		this.birthdayDate = birthdayDate;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	// other methods
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	} 
+	
+}
