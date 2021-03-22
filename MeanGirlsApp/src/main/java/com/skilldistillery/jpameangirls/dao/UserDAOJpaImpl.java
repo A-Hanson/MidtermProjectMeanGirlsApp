dda749c0877e86ef7e57b382b67782738c3d5c25
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-03-22 10:48:33 -0600

    create UserDAO interface, stub UserController, UserDAO impl

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/dao/UserDAOJpaImpl.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/dao/UserDAOJpaImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.jpameangirls.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -16,8 +18,45 @@ public class UserDAOJpaImpl implements UserDAO {
 	private EntityManager em;
 	
 	@Override
-	public User findById(int userId) {
-		return em.find(User.class, userId);
+	public User createUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findUserById(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findUserByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(int id, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
