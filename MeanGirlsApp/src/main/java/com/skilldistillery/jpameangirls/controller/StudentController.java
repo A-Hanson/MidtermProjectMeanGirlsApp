0c2cc691a34b462568d4ea96cb48e20041433d38
Sofia <sgkartist@gmail.com>
2021-03-22 10:50:30 -0600

    Student controller.

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controller/StudentController.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controller/StudentController.java
@@ -1,5 +1,25 @@
 package com.skilldistillery.jpameangirls.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class StudentController {
 
+	@Autowired
+	private StudentDAO studentDao;
+	
+	@RequestMapping(path= {"/","home.do"}, method = RequestMethod.GET)
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView();
+		  
+		mv.setViewName("index");
+		return mv; // (ViewResolver in use)
+	}
+	
 }
