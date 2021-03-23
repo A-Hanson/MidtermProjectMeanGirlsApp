c6f8236ca175931dab311ec7d5d72b2a4b5c29c5
Anna Hanson <>
2021-03-23 15:11:17 -0600

    create CommentVote DAO with methods

---
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/dao/CommentVoteDAO.java
@@ -0,0 +1,36 @@
+package com.skilldistillery.jpameangirls.dao;
+
+import java.util.List;
+
+import com.skilldistillery.jpameangirls.entities.Comment;
+import com.skilldistillery.jpameangirls.entities.CommentVote;
+import com.skilldistillery.jpameangirls.entities.Student;
+
+public interface CommentVoteDAO {
+
+//	CREATE
+	CommentVote create(CommentVote commentVote);
+	
+//	UPDATE
+	CommentVote update(int id, CommentVote commentVote);
+	
+//	DELETE
+	boolean delete(int id);
+	
+//	Aggregate Information
+	int commentTotalScore(int commentId);
+
+	int commentUpVoteTotal(int commentId);
+	
+	int commentDownVoteTotal(int commentId);
+	
+	int studentTotalScore(int studentId);
+	
+	int studentUpVoteTotal(int studentId);
+	
+	int studentDownVoteTotal(int studentId);
+	
+	List<Student> studentsWhoUpvoted(int commentId);
+	
+	List<Comment> commentsStudentUpVoted(int studentId);
+}
