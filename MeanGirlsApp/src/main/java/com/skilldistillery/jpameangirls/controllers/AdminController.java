b838f33c19839132f97da22f23d871bfa1914482
lisies93 <solorzanolisseth83@gmail.com>
2021-03-24 16:10:18 -0600

    CRUD for admin working

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/AdminController.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/AdminController.java
@@ -2,11 +2,14 @@ package com.skilldistillery.jpameangirls.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.jpameangirls.dao.UserDAO;
+import com.skilldistillery.jpameangirls.entities.User;
 
 @Controller
 public class AdminController {
@@ -46,6 +49,20 @@ public class AdminController {
 
 		return mv;
 	}
-		
+	
+	@RequestMapping(path="updateUser.do", method = RequestMethod.GET)
+	public String goToUpdatePage(int id, Model model) {
+		model.addAttribute("user", userDao.findUserById(id));
+		return "updateUser";
+	}
+	
+	
+	@RequestMapping(path="updateUser.do", method = RequestMethod.POST)
+	public String updateUserPage(int id, User user, RedirectAttributes redir, Model model) {
+		User u = null;
+		 u = userDao.updateUser(id, user);
+		 model.addAttribute("user", u);
+		return "singleUser";
+	}
 	
 }
