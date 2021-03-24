cc6fbec1d9e21f6d2e6548d6c2b6c66f7b564bae
Anna Hanson <>
2021-03-24 15:48:24 -0600

    refactor voting for multiple cliques

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/CommentController.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/CommentController.java
@@ -45,38 +45,74 @@ public class CommentController {
 		mv.setViewName("redirect:cafeteriaforum.do");
 		return mv;
 	}
-
-	@RequestMapping(path = "upVote.do", method = RequestMethod.POST)
-	public ModelAndView upVote(String studentId, String commentId, Boolean vote, RedirectAttributes redir) {
+	
+	@RequestMapping(path = "addPlasticsComment.do", method = RequestMethod.POST)
+	public ModelAndView addPlasticsComment(Comment comment, String studentId, RedirectAttributes redir) {
 		ModelAndView mv = new ModelAndView();
-		Integer intSId = Integer.parseInt(studentId);
-		Student student = studentDao.findById(intSId);
-		Integer intCId = Integer.parseInt(commentId);
-		Comment comment = commentDao.findById(intCId);
-		cvDao.create(student, comment, vote);
-		mv.setViewName("redirect:cafeteriaforum.do");
+		Integer integerId = Integer.parseInt(studentId);
+		
+		Student student = studentDao.findById(integerId);
+		Clique plastics = cliqueDao.findById(2);
+		
+		commentDao.create(comment, student, plastics);
+		redir.addFlashAttribute("plasticsComment", comment);
+		mv.setViewName("redirect:plasticsforum.do");
 		return mv;
 	}
 
-	@RequestMapping(path = "downVote.do", method = RequestMethod.POST)
-	public ModelAndView downVote(String studentId, String commentId, Boolean vote, RedirectAttributes redir) {
+	@RequestMapping(path = "vote.do", method = RequestMethod.POST)
+	public ModelAndView upVote(String studentId, String commentId, String cliqueId, Boolean vote, RedirectAttributes redir) {
 		ModelAndView mv = new ModelAndView();
-		Integer intSId = Integer.parseInt(studentId);
-		Student student = studentDao.findById(intSId);
-		Integer intCId = Integer.parseInt(commentId);
-		Comment comment = commentDao.findById(intCId);
+		int sId = Integer.parseInt(studentId);
+		Student student = studentDao.findById(sId);
+		int cId = Integer.parseInt(commentId);
+		Comment comment = commentDao.findById(cId);
 		cvDao.create(student, comment, vote);
-		mv.setViewName("redirect:cafeteriaforum.do");
+		int clId = Integer.parseInt(cliqueId);
+		switch (clId) {
+			case 1:
+				mv.setViewName("redirect:cafeteriaforum.do");
+				break;
+			case 2:
+				mv.setViewName("redirect:plasticsforum.do");
+				break;
+				
+		}
 		return mv;
 	}
+
+//	@RequestMapping(path = "downVote.do", method = RequestMethod.POST)
+//	public ModelAndView downVote(String studentId, String commentId, String cliqueId, Boolean vote, RedirectAttributes redir) {
+//		ModelAndView mv = new ModelAndView();
+//		int intSId = Integer.parseInt(studentId);
+//		Student student = studentDao.findById(intSId);
+//		int intCId = Integer.parseInt(commentId);
+//		Comment comment = commentDao.findById(intCId);
+//		cvDao.create(student, comment, vote);
+//		
+//		
+//		mv.setViewName("redirect:cafeteriaforum.do");
+//		return mv;
+//	}
 	
 	@RequestMapping(path="deleteComment.do", method = RequestMethod.POST)
-	public String deleteCafeteriaComment(String commentId, RedirectAttributes redir, Model model) {
-
+	public String deleteCafeteriaComment(String commentId, String cliqueId, RedirectAttributes redir, Model model) {
 		int cId = Integer.parseInt(commentId);
 		commentDao.softDelete(cId);
-		model.addAttribute("cafeteriaComments", commentDao.findCommentsInTheLast24HoursFromCliqueWithId(1));
-		return "redirect:cafeteriaforum.do";
+		String page = "";
+		int clId = Integer.parseInt(cliqueId);
+		switch (clId) {
+			case 1:
+				page = "redirect:cafeteriaforum.do";
+				model.addAttribute("cafeteriaComments", commentDao.findCommentsInTheLast24HoursFromCliqueWithId(1));
+				break;
+			case 2:
+				page = "redirect:plasticsforum.do";
+				model.addAttribute("plasticsComments", commentDao.findCommentsByCliqueId(2));
+				break;
+				
+		}
+		return page;
 	}
 	
 	@RequestMapping(path="updateComment.do", method = RequestMethod.GET)
@@ -88,10 +124,20 @@ public class CommentController {
 	
 	
 	@RequestMapping(path="updateComment.do", method = RequestMethod.POST)
-	public String updateCafteriaComment(String id, Comment comment, RedirectAttributes redir) {
+	public String updateCafteriaComment(String id, String cliqueId, Comment comment, RedirectAttributes redir) {
 		int commentId = Integer.parseInt(id);
 		commentDao.update(commentId, comment);
-		return "redirect:cafeteriaforum.do";
+		String page = "";
+		int clId = Integer.parseInt(cliqueId);
+		switch (clId) {
+			case 1:
+				page = "redirect:cafeteriaforum.do";
+				break;
+			case 2:
+				page = "redirect:plasticsforum.do";
+				break;
+		}
+		return page;
 	}
 	
 	
