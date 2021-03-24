09a3c0dfcaeddd89c1c360e33b8190326f72e2ae
lisies93 <solorzanolisseth83@gmail.com>
2021-03-24 17:41:16 -0600

    admin manage users working

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/AdminController.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/AdminController.java
@@ -35,7 +35,7 @@ public class AdminController {
 	public ModelAndView getUserById(int id) {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("idNum", id);
-		mv.addObject("user", userDao.findUserById(id));
+		mv.addObject("userLookUp", userDao.findUserById(id));
 		mv.setViewName("singleUser");
 		return mv;
 	}
@@ -44,7 +44,7 @@ public class AdminController {
 	public ModelAndView findUserByUsername(String username) {
 		ModelAndView mv = new ModelAndView();
         mv.addObject("username", username);
-		mv.addObject("user", userDao.findUserByUsername(username));
+		mv.addObject("userLookUp", userDao.findUserByUsername(username));
 		mv.setViewName("singleUser");
 
 		return mv;
@@ -52,7 +52,7 @@ public class AdminController {
 	
 	@RequestMapping(path="updateUser.do", method = RequestMethod.GET)
 	public String goToUpdatePage(int id, Model model) {
-		model.addAttribute("user", userDao.findUserById(id));
+		model.addAttribute("userLookUp", userDao.findUserById(id));
 		return "updateUser";
 	}
 	
@@ -61,8 +61,16 @@ public class AdminController {
 	public String updateUserPage(int id, User user, RedirectAttributes redir, Model model) {
 		User u = null;
 		 u = userDao.updateUser(id, user);
-		 model.addAttribute("user", u);
+		 model.addAttribute("userLookUp", u);
 		return "singleUser";
+	}
+	
+	@RequestMapping(path="deleteUser.do", method = RequestMethod.POST)
+	public String deleteUser(int id, RedirectAttributes redir, Model model) {
+
+		userDao.softDelete(id);
+		model.addAttribute("deletedUser", id);
+		return "redirect:getUser.do";
 	}
 	
 }
--- MeanGirlsApp/src/main/webapp/WEB-INF/singleUser.jsp
+++ MeanGirlsApp/src/main/webapp/WEB-INF/singleUser.jsp
@@ -6,28 +6,42 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>${user.firstName}</title>
+<title>${userLookUp.firstName}</title>
+<jsp:include page="head.jsp" />
 </head>
 <body>
 
 
+<jsp:include page="nav.jsp" />
+
+<div class="container">
+<c:out value="${loggedInUser }" />
 	<c:choose>
 
-		<c:when test="${empty user}">
+		<c:when test="${empty userLookUp}">
 			<h3>No user Was Found.</h3>
 		</c:when>
 
 		<c:otherwise>
 			
-  <h1>${user.firstName} ${user.lastName}</h1>
-   <p>${user.username}</p>
+  <h1>${userLookUp.firstName} ${userLookUp.lastName}</h1>
+   <p>${userLookUp.username}</p>
 
-
+   <div >
+			<form action="updateUser.do" method="GET">
+				<input type="hidden" name="id" value="${userLookUp.id}"/> 
+				<input type="submit" value="Update" />
+			</form>
+			<form action="deleteUser.do" method="POST">
+				<input type="hidden" name="id" value="${userLookUp.id}"/> 
+				<input type="submit" value="Delete" onclick="return confirm('Are you sure? This CANNOT be undone.'); " />
+			</form>
+</div>
   </c:otherwise>
   </c:choose>
-
+</div>
 
 		
-
+<jsp:include page="foot.jsp" />
 </body>
 </html>
