f0eef6cdff986d694e480078f1b5102a9dd30f9d
Anna Hanson <>
2021-03-24 13:50:55 -0600

    jpql queries now order comments newest to oldest

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/dao/CommentDAOImpl.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/dao/CommentDAOImpl.java
@@ -28,8 +28,8 @@ public class CommentDAOImpl implements CommentDAO {
 
 	@Override
 	public List<Comment> findAll() {
-		
-		return em.createQuery("select c from Comment c", Comment.class).getResultList();
+		String query = "select c from Comment c ORDER BY c.createdDate DESC";
+		return em.createQuery(query, Comment.class).getResultList();
 	}
 
 	@Override
@@ -82,8 +82,8 @@ public class CommentDAOImpl implements CommentDAO {
 
 	@Override
 	public List<Comment> findCommentsByCliqueId(int cliqueId) {
-		
-		return em.createQuery("SELECT c FROM Comment c JOIN c.clique cl WHERE cl.id = :id AND c.enabled=TRUE", Comment.class).setParameter("id", cliqueId).getResultList();
+		String query = "SELECT c FROM Comment c JOIN c.clique cl WHERE cl.id = :id AND c.enabled=TRUE ORDER BY c.createdDate DESC";
+		return em.createQuery(query, Comment.class).setParameter("id", cliqueId).getResultList();
 	}
 
 	@Override
@@ -93,7 +93,7 @@ public class CommentDAOImpl implements CommentDAO {
 //		I used the above query as a guideline.
 		
 		LocalDateTime yesterday = LocalDateTime.now().plusHours(24);
-		String query = "SELECT c FROM Comment c JOIN c.clique cl WHERE cl.id = :id AND c.enabled=TRUE AND c.createdDate >= :yesterday";
+		String query = "SELECT c FROM Comment c JOIN c.clique cl WHERE cl.id = :id AND c.enabled=TRUE AND c.createdDate >= :yesterday ORDER BY c.createdDate DESC";
 		return em.createQuery(query, Comment.class).setParameter("id", id).setParameter("yesterday", yesterday).getResultList();
 	}
 }
