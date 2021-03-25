7254f7edd329022f6ee7a444a5ef1661b08ebd16
Anna Hanson <>
2021-03-25 10:17:56 -0600

    fix spelling errors in request mapping

--- MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/CliqueController.java
+++ MeanGirlsApp/src/main/java/com/skilldistillery/jpameangirls/controllers/CliqueController.java
@@ -2,17 +2,22 @@ package com.skilldistillery.jpameangirls.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.jpameangirls.dao.CliqueDAO;
 import com.skilldistillery.jpameangirls.dao.CommentDAO;
 import com.skilldistillery.jpameangirls.dao.CommentVoteDAO;
+import com.skilldistillery.jpameangirls.dao.StudentDAO;
 import com.skilldistillery.jpameangirls.entities.Clique;
 import com.skilldistillery.jpameangirls.entities.Comment;
+import com.skilldistillery.jpameangirls.entities.Student;
 
 @Controller
 public class CliqueController {
@@ -25,6 +30,9 @@ public class CliqueController {
 
 	@Autowired
 	private CommentVoteDAO cvDao;
+	
+	@Autowired
+	private StudentDAO studentDao;
 
 	@RequestMapping(path = "cafeteriaforum.do")
 	public String index(Model model) {
@@ -39,17 +47,21 @@ public class CliqueController {
 	}
 	
 	@RequestMapping(path="plasticsform.do", method=RequestMethod.GET)
-	public String loadPlastics(Model model, String studentId) {
-		int sId = Integer.parseInt(studentId);
+	public ModelAndView loadPlastics(HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+
+		Student student = (Student) session.getAttribute("student");
+		student.setTotalFetch(cvDao.studentTotalScore(student.getId()));
+		session.setAttribute("student", student);
 		Clique plastics = cliqueDao.findById(2);
 		List<Comment> comments = commentDao.findCommentsByCliqueId(2);
 		for (Comment comment : comments) {
 			comment.setTotalFetch(cvDao.commentTotalScore(comment.getId()));
 		}
-		model.addAttribute("clique", plastics);
-		model.addAttribute("studentFetch", cvDao.studentTotalScore(sId));
-		model.addAttribute("plasticsComments", comments);
-		return "plastics";
+		mv.addObject("clique", plastics);
+		mv.addObject("plasticsComments", comments);
+		mv.setViewName("plastics");
+		return mv;
 	}
 	
 	@RequestMapping(path="addClique.do", method=RequestMethod.POST)
